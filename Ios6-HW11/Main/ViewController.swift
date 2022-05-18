3d5cb6db292e60d2c6d27640e233d7f5612dca00
Mogan <krsk.1994@mail.ru>
2022-05-18 23:51:12 +0400

    Background UIColor extension

--- Ios6-HW11/Main/ViewController.swift
+++ Ios6-HW11/Main/ViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+      }
+    }
 
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        v
+        
+        self.view.backgroundColor = UIColor.rgb(red: 25, green: 25, blue: 27)
         // Do any additional setup after loading the view.
     }
 
